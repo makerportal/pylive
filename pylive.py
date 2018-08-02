a07419fd3aa69efd09c4361f919c32ed8fb8ada0
Engineer's Maker Portal <40637421+engineersportal@users.noreply.github.com>
2018-08-02 15:38:17 -0400

    Update pylive.py

--- pylive.py
+++ pylive.py
@@ -1,20 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# use ggplot style for more sophisticated visuals
 plt.style.use('ggplot')
 
-def live_plotter(x_vec,y1_data,line1,identifier=''):
+def live_plotter(x_vec,y1_data,line1,identifier='',pause_time=0.1):
     if line1==[]:
+        # this is the call to matplotlib that allows dynamic plotting
         plt.ion()
         fig = plt.figure(figsize=(13,6))
         ax = fig.add_subplot(111)
-        line1, = ax.plot(x_vec,y1_data,'-o',alpha=0.8)
+        # create a variable for the line so we can later update it
+        line1, = ax.plot(x_vec,y1_data,'-o',alpha=0.8)        
+        #update plot label/title
         plt.ylabel('Y Label')
         plt.title('Title: {}'.format(identifier))
         plt.show()
-
-    line1.set_ydata(y1_data)
-    plt.ylim([np.min(y1_data)-np.std(y1_data),np.max(y1_data)+np.std(y1_data)])
-    plt.pause(0.1)
     
+    # after the figure, axis, and line are created, we only need to update the y-data
+    line1.set_ydata(y1_data)
+    # adjust limits to handle new data
+    plt.ylim([np.min(y1_data)-np.std(y1_data),np.max(y1_data)+np.std(y1_data)])
+    # this pauses the data so the figure/axis can catch up - the amount of pause can be altered above
+    plt.pause(pause_time)
+    
+    # return line so we can update it again in the next iteration
     return line1

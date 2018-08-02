c3166423d5c7acf20c3a3640f667609c503e3678
Engineer's Maker Portal <40637421+engineersportal@users.noreply.github.com>
2018-08-02 15:28:15 -0400

    Create random_plotter.py

---
+++ random_plotter.py
@@ -0,0 +1,12 @@
+from pylive import live_plotter
+import numpy as np
+
+size = 100
+x_vec = np.linspace(0,1,size+1)[0:-1]
+y_vec = np.random.randn(len(x_vec))
+line1 = []
+while True:
+    rand_val = np.random.randn(1)
+    y_vec[-1] = rand_val
+    line1 = live_plotter(x_vec,y_vec,line1)
+    y_vec = np.append(y_vec[1:],0.0)

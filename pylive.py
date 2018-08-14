d5ae8b38a48086d20662161aff42c738bd62a357
Engineer's Maker Portal <40637421+engineersportal@users.noreply.github.com>
2018-08-14 10:42:39 -0400

    Update pylive.py

--- pylive.py
+++ pylive.py
@@ -27,3 +27,23 @@ def live_plotter(x_vec,y1_data,line1,identifier='',pause_time=0.1):
     
     # return line so we can update it again in the next iteration
     return line1
+
+# the function below is for updating both x and y values (great for updating dates on the x-axis)
+def live_plotter_xy(x_vec,y1_data,line1,identifier='',pause_time=0.01):
+    if line1==[]:
+        plt.ion()
+        fig = plt.figure(figsize=(13,6))
+        ax = fig.add_subplot(111)
+        line1, = ax.plot(x_vec,y1_data,'r-o',alpha=0.8)
+        plt.ylabel('Y Label')
+        plt.title('Title: {}'.format(identifier))
+        plt.show()
+        
+    line1.set_data(x_vec,y1_data)
+    plt.xlim(np.min(x_vec),np.max(x_vec))
+    if np.min(y1_data)<=line1.axes.get_ylim()[0] or np.max(y1_data)>=line1.axes.get_ylim()[1]:
+        plt.ylim([np.min(y1_data)-np.std(y1_data),np.max(y1_data)+np.std(y1_data)])
+
+    plt.pause(pause_time)
+    
+    return line1
